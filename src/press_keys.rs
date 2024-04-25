374656a94086db5c6d4211003e70951d65840934
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2024-04-25 19:14:39 +0100

    Update enigo

--- src/press_keys.rs
+++ src/press_keys.rs
@@ -1,10 +1,10 @@
 use openaction::*;
 
-use enigo::{Enigo, KeyboardControllable};
+use enigo::{Enigo, DSL, Settings};
 
 pub fn key_down(event: KeyEvent) -> EventHandlerResult {
 	if let Some(value) = event.payload.settings.as_object().unwrap().get("down") {
-		let mut enigo = Enigo::new();
+		let mut enigo = Enigo::new(&Settings::default())?;
 		enigo.key_sequence_parse(value.as_str().unwrap());
 	}
 
@@ -13,7 +13,7 @@ pub fn key_down(event: KeyEvent) -> EventHandlerResult {
 
 pub fn key_up(event: KeyEvent) -> EventHandlerResult {
 	if let Some(value) = event.payload.settings.as_object().unwrap().get("up") {
-		let mut enigo = Enigo::new();
+		let mut enigo = Enigo::new(&Settings::default())?;
 		enigo.key_sequence_parse(value.as_str().unwrap());
 	}
 
